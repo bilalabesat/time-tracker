4aa04a1067e942a3f158bbb8b4cce9425b1714c5
bilalabesat <bilal.abesat@hotmail.com>
2019-12-02 14:24:25 -0500

    Update job.groovy

--- job.groovy
+++ job.groovy
@@ -10,7 +10,7 @@ job('TimeTracker Job') { //*note:NodeJS example is Job name*
 			scm('H/5 * * * *')
 		}
 		wrappers { // name of the maven installation (Manage Jenkins > Configure tools > NodeJS installations > name )
-		maven ('mvn')
+		maven('maven')
 		}
 		
 		steps { //*note: exec steps
